6290a6d91e965a4bf4a0a632afd7d74c92842f2e
Lewis Inder <lewisinder@gmail.com>
2026-07-05 18:15:19 +1200

    Strip Facebook tracking parameter

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,8 +1,18 @@
 export function onRequest(context) {
   const url = new URL(context.request.url);
+  let shouldRedirect = false;
 
   if (url.hostname === "www.paradisopizza.co.nz") {
     url.hostname = "paradisopizza.co.nz";
+    shouldRedirect = true;
+  }
+
+  if (url.searchParams.has("fbclid")) {
+    url.searchParams.delete("fbclid");
+    shouldRedirect = true;
+  }
+
+  if (shouldRedirect) {
     return Response.redirect(url.toString(), 301);
   }
 
